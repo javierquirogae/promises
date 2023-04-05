5dc688fd20c38381ae8097b045ad796b00fb7188
jav <javier.quiroga.e@gmail.com>
2023-04-05 00:32:12 -0400

    the 'numbers' portion is complete

--- number_facts/app.js
+++ number_facts/app.js
@@ -3,23 +3,73 @@ $res1 = $('#res1');
 $res2 = $('#res2');
 
 $form1 = $('#form1');
-let num = 0;
+$form2 = $('#form2');
+
 
 $res.append('<h3>Get cool facts about your favorite numbers !!</h3>');
 
 $form1.submit(function(e) {
     e.preventDefault();
+    let num = 0;
     $res1.empty();
     num = $('#number').val();
     console.log(num);
-    let url = `http://numbersapi.com/${num}?json`
-    console.log(url);
-    axios
-    .get(url)
-    .then(res => {
-        $res1.append(`<h3 style="color:blue;">${res.data.text}</h3>`);
+    getFourFacts(num);
+});
+
+function getFourFacts(num) {
+    let fourPromises = [];
+    let url = '';
+    for (let i = 0; i < 4; i++) {
+        url = `http://numbersapi.com/${num}?json`;
+        fourPromises.push(axios.get(url));
+        console.log(url);
+    }
+    Promise.all(fourPromises)
+    .then(facts => {
+        facts.forEach(res => {
+            $res1.append(`<h3 style="color:blue;">${res.data.text}</h3>`);
+        });
     })
     .catch(err => {
         $res1.append(`<p style="color:red;"><b>${err}</b></p>`);
-    }); 
+    });
+}
+
+
+
+$form2.submit(function(e) {
+    e.preventDefault();
+    let choices = [];
+    $res2.empty();
+    choices.push($('#number1').val());
+    choices.push($('#number2').val());
+    choices.push($('#number3').val());
+    console.log(choices);
+    getThreeFacts(choices);
 });
+
+
+    
+
+function getThreeFacts(nums) {
+    let threePromises = [];
+    let url = '';
+    for (let i = 0; i < 3; i++) {
+        url = `http://numbersapi.com/${nums[i]}?json`;
+        threePromises.push(axios.get(url));
+        console.log(url);
+    }
+    Promise.all(threePromises)
+    .then(facts => {
+        facts.forEach(res => {
+            $res2.append(`<h3 style="color:blue;">${res.data.text}</h3>`);
+        });
+    })
+    .catch(err => {
+        $res2.append(`<p style="color:red;"><b>${err}</b></p>`);
+    });
+}
+
+
+
